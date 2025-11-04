89200fcd341a8c2ae0887fd9298f7dd000ca1254
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2025-11-04 16:21:24 -0300

    Remove custom theme arg

--- src/theme/get-pulga-theme.ts
+++ src/theme/get-pulga-theme.ts
@@ -1,14 +1,6 @@
 import { createTheme, Theme } from "@mui/material";
 import theme from "./theme.mui";
 
-const omitComponents = (customTheme?: Partial<Theme>): Partial<Theme> => {
-  const { components, ...rest } = customTheme || {};
-  return rest;
-};
-
-export const getPulgaTheme = (customTheme?: Partial<Theme>): Theme => {
-  return createTheme({
-    ...theme,
-    ...omitComponents(customTheme),
-  });
-};
+export const getPulgaTheme = (): Theme => {
+  return createTheme(theme);
+}
