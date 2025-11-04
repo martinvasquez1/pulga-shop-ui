cc9d5920347a38832d3719b50c1086aee6083c17
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2025-11-04 13:18:02 -0300

    Skip components for custom theme

--- src/theme/get-pulga-theme.ts
+++ src/theme/get-pulga-theme.ts
@@ -1,9 +1,14 @@
 import { createTheme, Theme } from "@mui/material";
 import theme from "./theme.mui";
 
+const omitComponents = (customTheme?: Partial<Theme>): Partial<Theme> => {
+  const { components, ...rest } = customTheme || {};
+  return rest;
+};
+
 export const getPulgaTheme = (customTheme?: Partial<Theme>): Theme => {
   return createTheme({
     ...theme,
-    ...customTheme,
+    ...omitComponents(customTheme),
   });
 };
